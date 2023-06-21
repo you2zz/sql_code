330f0218b394ae15916da24582a4e9ce91d3beb8
you2zz <yri_vla@mail.ru>
2023-06-21 16:56:48 +0500

    initial

---
+++ 01/music_servise.sql
@@ -0,0 +1,46 @@
+CREATE TABLE IF NOT EXISTS Genre (
+    genre_id SERIAL PRIMARY KEY,
+    genre_name VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Artist (
+    artist_id SERIAL PRIMARY KEY,
+    artist_name VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Album (
+    album_id SERIAL PRIMARY KEY,
+    album_title VARCHAR(250) NOT NULL,
+    year_of_release date NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Track (
+    track_id SERIAL PRIMARY KEY,
+    track_name VARCHAR(250) NOT NULL,
+    track_duration INTEGER NOT NULL,
+    album_id INTEGER REFERENCES Album (album_id)
+);
+
+CREATE TABLE IF NOT EXISTS Collections (
+    collections_id SERIAL PRIMARY KEY,
+    collections_title VARCHAR(250) NOT NULL,
+    year_of_release date NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS genre_artist (
+    atrist_id INTEGER NOT NULL REFERENCES Artist (artist_id),
+    genre_id INTEGER NOT NULL REFERENCES Genre (genre_id),
+    CONSTRAINT genre_artist_pk PRIMARY KEY (atrist_id, genre_id)
+);
+
+CREATE TABLE IF NOT EXISTS artist_album (
+    atrist_id INTEGER NOT NULL REFERENCES Artist (artist_id),
+    album_id INTEGER NOT NULL REFERENCES Album (album_id),
+    CONSTRAINT artist_album_pk PRIMARY KEY (atrist_id, album_id)
+);
+
+CREATE TABLE IF NOT EXISTS track_collections (
+    track_id INTEGER NOT NULL REFERENCES Track (track_id),
+    collections_id INTEGER NOT NULL REFERENCES Collections (collections_id),
+    CONSTRAINT track_collections_pk PRIMARY KEY (track_id, collections_id)
+);
