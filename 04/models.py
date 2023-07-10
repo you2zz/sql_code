84a2dde7f2ebd90009d4dea8da1fae4571f08199
you2zz <yri_vla@mail.ru>
2023-07-10 17:25:48 +0500

    10072023_1725

--- 04/models.py
+++ 04/models.py
@@ -3,6 +3,47 @@ from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
+class Publisher(Base):
+    __tablename__ = "publisher"
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=40), unique=True, nullable=False)
+    def __str__(self):
+        return f'Publisher {self.id}: {self.name}'
+
+class Stock(Base):
+    __tablename__ = "stock"
+    id = sq.Column(sq.Integer, primary_key=True)
+    id_book = sq.Column(sq.Integer, sq.ForeignKey("book.id", nullable=False))
+    id_shop = sq.Column(sq.Integer, sq.ForeignKey("shop.id", nullable=False))
+    count = sq.Column(sq.Integer, nullable=False)
+
+class Book(Base):
+    __tablename__ = "book"
+    id = sq.Column(sq.Integer, primary_key=True)
+    title = sq.Column(sq.String(length=250), nullable=False)
+    id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publisher.id", nullable=False))
+    publisher = relationship("Publisher", backref="book")
+    shop = relationship("Stock", backref="book")
+    def __str__(self):
+        return f'Book {self.id}: {self.title}'
+    
+class Shop(Base):
+    __tablename__ = "shop"
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=100), unique=True, nullable=False)
+    book = relationship("Stock", backref="shop")
+    def __str__(self):
+        return f'Shop {self.id}: {self.name}'
+    
+class Sale(Base):
+    __tablename__ = "sale"
+    id = sq.Column(sq.Integer, primary_key=True)
+    prise = 
+
+
+
+
+
 class Course(Base):
     __tablename__ = "course" # это название таблицы
 
